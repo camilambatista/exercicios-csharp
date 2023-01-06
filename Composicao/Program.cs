fa60ff40d0279d91186f21bd9673afd3df778d0d
Camila Batista <camissk@hotmail.com>
2023-01-06 16:53:44 -0300

    Adicionando enums e composicao

---
+++ Composicao/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using Composicao.Entities;
+using Composicao.Entities.Enums;
+
+namespace Composicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to tis worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime dateTime = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                worker.AddContract(new HourContract(dateTime, valuePerHour, hours));
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            double income = worker.Income(date.Year, date.Month);
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Department: {worker.Department.Name}");
+            Console.WriteLine($"Income for {date.Month}/{date.Year}: {income.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enums.Entities;
+using Enums.Entities.Enums;
+
+namespace Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Shipped
+            };
+
+            Console.WriteLine(order);
+
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
